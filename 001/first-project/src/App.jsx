b1a56e44d30618a55c88c3447e5daa698a684bc4
ToKolob <111467715+ToKolob@users.noreply.github.com>
2023-10-02 10:39:56 -0300

    Initial commit

---
+++ 001/first-project/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App(){
+  return(
+    <div>
+     <h1>Hello, React!</h1>
+     <p>Para ser sincero ainda não ví vantagem de usar React</p>
+     <h2>As vantagens do react</h2>
+     <p>Vantagens do React</p>
+     
+    </div>
+
+)  
+}
---
+++ 002-fist-exercise/src/App.jsx
@@ -0,0 +1,29 @@
+export default function App(){
+  return(
+    <div className="App">
+      <img src="/react.png" alt="" />
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuario web e nativas.</p>
+      <button>Aprenda react</button><button>Referencia da API</button>
+      <hr />
+      <h2>Crie interfaces de usuario de componentes</h2>
+      <p>React permite criar interfaces de usuario de componentes para suas aplicações, apartir de componentes básicos.</p>
+      <hr /> 
+      <h2>escreva componentes com codigo de marcaçao</h2>
+      <p>Componentes React são criados com JSX, uma sintaxe de marcação para criar componentes de aplicação, como por exemplo, o componente <code>h1</code> ou <code>h2</code>.</p>     
+      <hr />
+      <h2>Proximos passos</h2>
+      <ul>
+        <li>uso de dados dinamicos</li>
+        <li>craiçao de componentes</li>
+        <li>estilizacao de componentes</li>
+        <li>reutiliçao de componentes</li>
+        <li>uso de props e children</li>
+        <li>uso de hooks</li>
+        <li>uso de estado</li>
+        <li>uso de context</li>
+
+
+      </ul>
+    </div>)
+}
